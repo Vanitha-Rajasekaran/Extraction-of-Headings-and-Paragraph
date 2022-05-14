2bc876cdd2ce7f402a7ef754ab8aa87613ef35df
Vanitha-Rajasekaran <100250547+Vanitha-Rajasekaran@users.noreply.github.com>
2022-05-14 12:37:38 +0530

    Update extract.py

--- extract/extract.py
+++ extract/extract.py
@@ -7,6 +7,7 @@ def text_extraction(pdf):
     for pages in range(0,len(pdf.pages)):
         page = pdf.pages[pages]
         text += page.extract_text()
+        
     return text
     
 def heading_extraction(text):
@@ -30,8 +31,8 @@ def keyword(text):
     keyword_pos = re.finditer(keyword,text)
     return keyword_pos
     
-def search(keyword_pos,span,text):
-    flag = 0
+def search(keyword_pos,span,text,dicts,totalpages,filename):
+    flag = before = after = 0
     content = ''
     for p in keyword_pos:
         p = p.span()
@@ -43,18 +44,60 @@ def search(keyword_pos,span,text):
         if flag == after:
             continue
         if before == after:
-            content += '\n'+text[after[0]:]
+            p = dicts[after]["page"]
+            if totalpages-p == 0:
+              content += '\n Found on Page : '+str(p)+'\n'+text[after[0]:]
+            else:
+              content += '\n Found on Pages : '+str(p)+' - '+str(totalpages)+'\n'+text[after[0]:]
+            #tableextraction(p,totalpages,filename)
         else:
-            content += '\n'+text[before[0]:after[0]]
+            p1 = dicts[before]["page"]
+            p2= dicts[after]["page"]
+            if p2-p1 > 0 :
+              content += '\n Found on Pages : '+str(p1)+' - '+str(p2)+'\n'+text[before[0]:after[0]]
+            else:
+              content += '\n Found on Pages : '+str(p1)+'\n'+text[before[0]:after[0]]
+            #tableextraction(p1,p2,filename)
         flag = after
     return content
-    
+
+def page_finder(dicts,span):
+  for pages in range(0,len(pdf.pages)):
+        page = pdf.pages[pages]
+        text = page.extract_text()
+        for i in range(len(span)):
+          d = dicts[span[i]]["head"]
+          if d in text:
+            dicts[span[i]]["page"] = pages+1
+  return dicts 
+  
+def tableextraction(p1,p2,filename):
+  tables =camelot.read_pdf(filename,flavor="lattice",pages='all')
+  if tables.n > 0:
+    for i in range(0,tables.n):
+      temp = tables[i].shape
+      if temp[1] == 1:
+        continue
+      else:
+        header = tables[i].df.iloc[0]
+        new_df = pd.DataFrame(tables[i].df.values[1:],columns=header)
+        #new_df = new_df.replace('\n',' ', regex=True)
+        print(new_df)
+        
 #main
+dicts = {}
 filename = input("Enter a filename : ")
 pdf = pdfplumber.open(filename)
+totalpages=len(pdf.pages)
 text = text_extraction(pdf)
 heading = heading_extraction(text)
 span = span_of_heading(heading,text)
+for i in range(len(span)):
+  j = span[i]
+  dicts[j] = {}
+  head = heading[i]
+  dicts[j]["head"] = head
+dicts = page_finder(dicts,span)
 keyword_pos = keyword(text)
-content = search(keyword_pos,span,text)
+content = search(keyword_pos,span,text,dicts,totalpages,filename)
 print(content)
